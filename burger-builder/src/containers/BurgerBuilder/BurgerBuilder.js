4e9008a537e1bb62ea8be30af76bbd2f2d9dc537
gabrielgolzman <gabrielgolzman@gmail.com>
2021-02-13 13:55:07 -0300

    Creado diseño de la hamburguesa

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 3,
+			cheese: 2,
+			meat: 2,
+		},
+	};
 	render() {
 		return (
 			<div>
-				<div>Burger</div>
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Controls</div>
 			</div>
 		);
